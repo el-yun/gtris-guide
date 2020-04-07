ce866d3ea04f23b9d3b2e304fb4c1744cfcef63c
leejeongkyu <ljk@gabia.com>
2020-04-07 17:20:58 +0900

    툴팁 , 인풋 가이드 스타일 맞추기

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,81 +1,137 @@
 <template>
   <section>
     <header>
-      Tooltip
+      Input
+      <p class="contents-header-desc">Input user guide</p>
     </header>
-    <p class="contents-header-desc">button user guide</p>
+
     <div class="body-components">
-      <!-- preview Tooltip tag -->
-      <p class="gt-panel-title">preview</p>
-      <div class="gt-panel">
-        <div class="gt-panel-header">
-          <div class="circles">
-            <span class="circle circle-red"></span>
-            <span class="circle circle-yellow"></span>
-            <span class="circle circle-green"></span>
-          </div>
-        </div>
-        <div class="gt-panel-body" style="padding:10px;">
-
-          <div class="ui-wrap">
-            <gt-input placeholder="Basic usage" @pressEnter="$_pressEnter" @keyup="$_keyup" />
-          </div>
-          <div class="ui-wrap">
-            <gt-input defaultValue="기본 값입니다." placeholder="Basic usage" />
-          </div>
-          <div class="ui-wrap">
-            <gt-input size="sm" placeholder="small size" />
-            <br><br>
-            <gt-input placeholder="default size" />
-            <br><br>
-            <gt-input size="lg" placeholder="large size" />
-          </div>
-          <div class="ui-wrap">
-            <gt-input custom-class="hiworks-input-custom-class" placeholder="Custom Class: !important를 붙여야 css가 적용됨^^;;" />
-          </div>
-          <div class="ui-wrap">
-            <gt-input placeholder="width 값 조정" width="500px" />
-          </div>
-          <div class="ui-wrap">
-            <gt-input placeholder="height 값 조정" height="100px" />
-          </div>
-          <div class="ui-wrap">
-            <gt-input disabled placeholder="disabled" />
-          </div>
-
-        </div>
+      <h2 class="group-title">Usage</h2>
+      <!-- Basic -->
+      <div class="group">
+        <gt-panel>
+          <template #title>Basic</template>
+          <template #body>
+            <div style="padding:10px;">
+              <div class="ui-wrap">
+                <gt-input placeholder="Basic usage" @pressEnter="$_pressEnter" @keyup="$_keyup" />
+              </div>
+              <div class="ui-wrap">
+                <gt-input defaultValue="기본 값입니다." placeholder="Basic usage" />
+              </div>
+              <div class="ui-wrap">
+                <gt-input size="sm" placeholder="small size" />
+                <br><br>
+                <gt-input placeholder="default size" />
+                <br><br>
+                <gt-input size="lg" placeholder="large size" />
+              </div>
+              <div class="ui-wrap">
+                <gt-input custom-class="hiworks-input-custom-class" placeholder="Custom Class:!important 필요" />
+              </div>
+              <div class="ui-wrap">
+                <gt-input placeholder="width 값 조정" width="500px" />
+              </div>
+              <div class="ui-wrap">
+                <gt-input placeholder="height 값 조정" height="100px" />
+              </div>
+              <div class="ui-wrap">
+                <gt-input disabled placeholder="disabled" />
+              </div>
+            </div>
+          </template>
+        </gt-panel>
+        <prism-editor
+          :lineNumbers="true"
+          :code="code_basic"
+          :readonly="true"
+          language="html"
+        ></prism-editor>
+        <br>
+        <p>css</p>        
+        <prism-editor
+          :lineNumbers="true"
+          :code="code_basic_css"
+          :readonly="true"
+          language="css"
+        ></prism-editor>
+        
       </div>
-      <div class="gist">
-        <div class="gist-code">
-          <div class="gist-data">
-            <textarea :value="basic_btn_str" disabled style="height: 460px;" />
-          </div>
-        </div>
-      </div>
+
+      <h2 class="group-title">Props</h2>
+      <!-- gt-collapse  -->
+      <div class="group">        
+        <table class="group-table">
+          <colgroup>
+            <col style="width: 20%" />
+            <col style="width: 20%" />
+            <col style="width: 20%" />
+            <col style="width: 40%" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>OPTION</th>
+              <th>TYPE</th>
+              <th>DEFAULT</th>
+              <th>DESCRIPTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>defaultValue</td>
+              <td>String</td>
+              <td>''</td>
+              <td>required : false</td>
+            </tr>
+            <tr>
+              <td>defaultValue</td>
+              <td>String</td>
+              <td>''</td>
+              <td>required : false</td>
+            </tr>
+            <tr>
+              <td>placeholder</td>
+              <td>String</td>
+              <td>''</td>
+              <td>required : false</td>
+            </tr>
+            <tr>
+              <td>size</td>
+              <td>String</td>
+              <td>''</td>
+              <td>required : false</td>
+            </tr>                        
+            <tr>
+              <td>customClass</td>
+              <td>String</td>
+              <td>''</td>
+              <td>required : false</td>
+            </tr>  
+
+            <tr>
+              <td>height</td>
+              <td>String</td>
+              <td>''</td>
+              <td>required : false</td>
+            </tr>  
+
+            <tr>
+              <td>width</td>
+              <td>String</td>
+              <td>''</td>
+              <td>required : false</td>
+            </tr>              
 
 
-      <!-- preview Tooltip props  -->
-      <p class="gt-panel-title">props</p>
-      <div class="gt-panel">
-        <div class="gt-panel-header">
-          <div class="circles">
-            <span class="circle circle-red"></span>
-            <span class="circle circle-yellow"></span>
-            <span class="circle circle-green"></span>
-          </div>
-        </div>
-        <div class="gt-panel-body">
-          <pre>
-  defaultValue: {type: String, required: false, default: ''},
-  placeholder: {type: String, required: false, default: ''},
-  size: {type: String, required: false, default: ''},
-  customClass: { type: String, required: false, default: '' },
-  width: { type: String, required: false, default: '' },
-  height: { type: String, required: false, default: '' },
-  autofocus: { type: Boolean, required: false, default: false },
-          </pre>
+            <!-- <tr> 
+              <td>autofocus</td>
+              <td>Boolean</td>
+              <td>''</td>
+              <td>required : false</td>
+            </tr>   -->
 
-        </div>
+          </tbody>
+        </table>
       </div>
     </div>
   </section>
@@ -83,11 +139,10 @@
 
 <script>
 export default {
-  name: "tooltip-test",
+  name: "input-guide",
   data() {
     return {
-      basic_btn_str: `
-<div class="ui-wrap">
+      code_basic: `<div class="ui-wrap">
   <gt-input placeholder="Basic usage" @pressEnter="$_pressEnter" @keyup="$_keyup" />
 </div>
 <div class="ui-wrap">
@@ -101,7 +156,7 @@ export default {
   <gt-input size="lg" placeholder="large size" />
 </div>
 <div class="ui-wrap">
-  <gt-input custom-class="hiworks-input-custom-class" placeholder="Custom Class: !important를 붙여야 css가 적용됨^^;;" />
+  <gt-input custom-class="hiworks-input-custom-class" placeholder="Custom Class:!important 필요" />
 </div>
 <div class="ui-wrap">
   <gt-input placeholder="width 값 조정" width="500px" />
@@ -112,6 +167,11 @@ export default {
 <div class="ui-wrap">
   <gt-input disabled placeholder="disabled" />
 </div>
+`,
+code_basic_css:`
+.hiworks-input-custom-class {
+  border: 4px solid orangered !important;
+}
 `
     };
   },
@@ -127,6 +187,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.hiworks-input-custom-class {
+  border: 4px solid orangered !important;
+}
+
 section {
   header {
     font-weight: 400;
