b575c9832e515845e39afecbbf270e0459e4fc9e
Jhyoon <jhyoon@gabia.com>
2020-05-19 17:49:15 +0900

    저장소 api 파싱

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,24 @@
 
 <script>
 export default {
-  name: "gtris",
+  name: 'gtris',
   data() {
     return {};
   },
-  methods: {}
+  created() {
+    this.$_getMenu();
+  },
+  methods: {
+    async $_getMenu() {
+      await this.$store.dispatch('Map/SET_MAP');
+      await this.$store.dispatch('Map/SET_TAG');
+      //console.log(this.$store.getters[''])
+      await console.log(
+        this.$store.getters['Map/getComponentsMenu'],
+        this.$store.getters['Map/getVersion'],
+      );
+    },
+  },
 };
 </script>
 
