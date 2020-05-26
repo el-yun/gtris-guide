c41c2f794e52db9a44a1e805901e751f629452f4
Jhyoon <jhyoon@gabia.com>
2020-05-26 16:58:16 +0900

    publicpath 수정

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 module.exports = {
   outputDir: path.resolve(__dirname, "./docs"),
+  publicPath: "/gtris-guide/",
   pages: {
     gtris3: {
       entry: "src/main.js",
